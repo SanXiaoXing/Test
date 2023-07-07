a0e757991a180f76f7f94c665a917f8ce8322760
SanXiaoXing <1137199378@qq.com>
2023-07-07 11:31:11 +0800

    最新学习信息

--- vue-project/src/views/element/ElementView.vue
+++ vue-project/src/views/element/ElementView.vue
@@ -1,42 +1,320 @@
 <template>
-    <dev>
-        <el-row>
-            <el-button icon="el-icon-search" circle></el-button>
-            <el-button type="primary" icon="el-icon-edit" circle></el-button>
-            <el-button type="success" icon="el-icon-check" circle></el-button>
-            <el-button type="info" icon="el-icon-message" circle></el-button>
-            <el-button type="warning" icon="el-icon-star-off" circle></el-button>
-            <el-button type="danger" icon="el-icon-delete" circle></el-button>
-          </el-row>
+  <!-- 轮播图 -->
+  <div class="block">
+    <div class="carousel-container">
+      <el-carousel :interval="4000" type="card" height="400px">
+        <el-carousel-item v-for="item in 8" :key="item">
+          <h3 class="medium">{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
 
-          <el-carousel :interval="4000" type="card" height="200px">
-            <el-carousel-item v-for="item in 6" :key="item">
-              <h3 class="medium">{{ item }}</h3>
-            </el-carousel-item>
-          </el-carousel>
-    </dev>
+    <!-- 基础按钮 -->
+    <el-row justify="center">
+      <el-button icon="el-icon-search" circle></el-button>
+      <el-button type="primary" icon="el-icon-edit" circle></el-button>
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+      <el-button type="info" icon="el-icon-message" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </el-row>
+
+    <!-- 日历 -->
+    <el-calendar
+      v-model="value"
+      style="width: 700px; height: 600px"
+    ></el-calendar>
+
+    <!-- 表格 -->
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
+
+    <!-- 翻页效果 -->
+    <el-pagination
+      background
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      layout="sizes, prev, pager, next, jumper "
+      :total="1000"
+    >
+    </el-pagination>
+
+    <!-- 上传按钮 -->
+    <el-upload
+      class="upload-demo"
+      action="https://jsonplaceholder.typicode.com/posts/"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :before-remove="beforeRemove"
+      multiple
+      :limit="3"
+      :on-exceed="handleExceed"
+      :file-list="fileList"
+    >
+      <el-button size="big" type="primary">点击上传</el-button>
+      <div slot="tip" class="el-upload__tip">
+        只能上传jpg/png文件，且不超过500kb
+      </div>
+    </el-upload>
+
+    <!-- Dialog对话框 -->
+    <el-button type="text" @click="dialogTableVisible = true"
+      >测试Dialog</el-button
+    >
+    <el-dialog title="测试用例" :visible.sync="dialogTableVisible">
+      <el-form :model="form">
+        <el-form-item label="活动名称" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="活动地区" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="呀呼~选择地区">
+            <el-option label="Beijing" value="beijing"></el-option>
+            <el-option label="Shanghai" value="shanghai"></el-option>
+            <el-option label="Henan" value="henan"></el-option>
+            <el-option label="Shandong" value="shandong"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogTableVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogTableVisible = false"
+          >确 定</el-button
+        >
+      </div>
+    </el-dialog>
+
+    <!-- Dialog对话框-Form表单 -->
+    <el-button type="text" @click="dialogFormVisible = true"
+      >Dialog的Form表单</el-button
+    >
+    <el-dialog title="Form表单测试" :visible.sync="dialogFormVisible">
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="测试名称" prop="name">
+          <el-input v-model="ruleForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="测试范围" prop="region">
+          <el-select v-model="ruleForm.region" placeholder="请选择测试范围">
+            <el-option label="范围一" value="shanghai"></el-option>
+            <el-option label="范围二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="测试时间" required>
+          <el-col :span="11">
+            <el-form-item prop="date1">
+              <el-date-picker
+                type="date"
+                placeholder="选择日期"
+                v-model="ruleForm.date1"
+                style="width: 100%"
+              ></el-date-picker>
+            </el-form-item>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-form-item prop="date2">
+              <el-time-picker
+                placeholder="选择时间"
+                v-model="ruleForm.date2"
+                style="width: 100%"
+              ></el-time-picker>
+            </el-form-item>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="误差是否较大" prop="delivery">
+          <el-switch v-model="ruleForm.delivery"></el-switch>
+        </el-form-item>
+        <el-form-item label="测试性质" prop="type">
+          <el-checkbox-group v-model="ruleForm.type">
+            <el-checkbox label="线上远程测试" name="type"></el-checkbox>
+            <el-checkbox label="线下实体测试" name="type"></el-checkbox>
+            <el-checkbox label="线下视频教学" name="type"></el-checkbox>
+            <el-checkbox label="线上视频教学" name="type"></el-checkbox>
+            <el-checkbox label="线上半实体试验台" name="type"></el-checkbox>
+            <el-checkbox label="计算机实验平台" name="type"></el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="特殊资源" prop="resource">
+          <el-radio-group v-model="ruleForm.resource">
+            <el-radio label="线上辅助"></el-radio>
+            <el-radio label="线下教学"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="活动意见" prop="desc">
+          <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')"
+            >立即创建</el-button
+          >
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-export default{
+export default {
+  data() {
+    return {
+      ruleForm: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
+      rules: {
+        name: [
+          { required: true, message: "请输入活动名称", trigger: "blur" },
+          {
+            min: 3,
+            max: 20,
+            message: "长度在 3 到 20 个字符",
+            trigger: "blur",
+          },
+        ],
+        region: [
+          { required: true, message: "请选择活动区域", trigger: "change" },
+        ],
+        date1: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择日期",
+            trigger: "change",
+          },
+        ],
+        date2: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择时间",
+            trigger: "change",
+          },
+        ],
+        type: [
+          {
+            type: "array",
+            required: true,
+            message: "请至少选择一个活动性质",
+            trigger: "change",
+          },
+        ],
+        resource: [
+          { required: true, message: "请选择活动资源", trigger: "change" },
+        ],
+        desc: [{ required: true, message: "请填写活动形式", trigger: "blur" }],
+      },
 
-}
+      value: new Date(),
+
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
+      formLabelWidth: "120px",
+    };
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("提交成功");
+        } else {
+          console.log("提交失败，再去看看吧！！");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
 </script>
 
 <style>
-    .el-carousel__item h3 {
-        color: #475669;
-        font-size: 14px;
-        opacity: 0.75;
-        line-height: 200px;
-        margin: 0;
-    }
-  
-    .el-carousel__item:nth-child(2n) {
-      background-color: #99a9bf;
-    }
-    
-    .el-carousel__item:nth-child(2n+1) {
-      background-color: #d3dce6;
-    }
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+.block {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.block > * {
+  margin-bottom: 20px;
+  padding: 20px;
+  border-bottom: 0px solid #ccc;
+  border-radius: 5px;
+}
+.block > *:last-child {
+  border-bottom: none;
+  margin-bottom: 0;
+}
+.carousel-container {
+  width: 100%;
+  height: 500px;
+}
 </style>
