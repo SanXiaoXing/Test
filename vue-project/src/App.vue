32fa5a7117b0d3fa1052aa8f967ddfbe10188c41
SanXiaoXing <1137199378@qq.com>
2023-07-07 08:36:54 +0800

    Add New

---
+++ vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <element-view></element-view>
+  </div>
+</template>
+
+<script>
+import ElementView from './views/element/ElementView.vue';
+export default {
+  components: { ElementView },
+    data() {
+        return {
+            message: "hello"
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<style>
+
+</style>
